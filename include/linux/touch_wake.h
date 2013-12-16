79179b4db9d6352c44d04024c2f8fd63a66d5065
DerTeufel <dominik-kassel@gmx.de>
2013-12-16 13:33:17 +0530

    smdk4412: TouchWake
    Change-Id: I523c328bdc435311064168e53c4fe61da105e398

---
+++ include/linux/touch_wake.h
@@ -0,0 +1,17 @@
+/* include/linux/touch_wake.h */
+
+#ifndef _LINUX_TOUCH_WAKE_H
+#define _LINUX_TOUCH_WAKE_H
+
+#include <linux/input.h>
+
+void powerkey_pressed(void);
+void powerkey_released(void);
+void proximity_detected(void);
+void proximity_off(void);
+void touch_press(void);
+int get_touchoff_delay(void);
+bool device_is_suspended(void);
+void set_powerkeydev(struct input_dev * input_device);
+
+#endif
